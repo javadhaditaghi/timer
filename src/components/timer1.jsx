d6a048e718dfdbe8aa27f3dd382730f61a033528
javad <javadht1990a@gmail.com>
2024-01-17 00:19:21 +0330

    Add the first  timer

--- src/components/timer1.jsx
+++ src/components/timer1.jsx
@@ -1,9 +1,26 @@
+import { useState } from "react";
 import styles from "./timer1.module.css";
 
 
-const TimerA = () => {
+const TimerA = (props) => {
+
+    const [time, setTime] = useState(props.time);
+    const d = new Date();
+    const second = d.getSeconds().toString();
+    const minute = d.getMinutes().toString();
+    const hour = d.getHours().toString();
+
+    let timeHandler = () => {
+
+        // let day = d.getDate();
+        setTime(`${hour}:${minute}:${second}`)
+    }
+
     return (
-        pass
+        <div>
+            <h1>{time}</h1>
+            <button onClick={timeHandler}>Get Time</button>
+        </div>
     )
 }
 
