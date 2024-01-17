d11f6bc75b618035f9e1ee0c20f6295657701a91
javad <javadht1990a@gmail.com>
2024-01-17 22:45:41 +0330

    Add exercise 3: importing and exporting the functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import TimerA from './components/timer1'
 import TimerB from './components/timer2'
+import { Addition, Subtraction, Multiplication, Division, mainNum } from "./components/funcs"
 
 const App = () => {
 
@@ -8,6 +9,17 @@ const App = () => {
     <>
       <TimerA time="TIME"></TimerA>
       <TimerB></TimerB>
+      <div className='list-wrapper'>
+        <ul>
+          <h3>Exercise 3</h3>
+          <li>{Addition()}</li>
+          <li>{Subtraction()}</li>
+          <li>{Multiplication()}</li>
+          <li>{Division()}</li>
+
+        </ul>
+
+      </div>
 
     </>
   )
---
+++ src/components/funcs.jsx
@@ -0,0 +1,18 @@
+const mainNum = 10
+
+
+const Addition = () => {
+    return mainNum + mainNum
+}
+const Subtraction = () => {
+    return mainNum - mainNum
+}
+const Multiplication = () => {
+    return mainNum * mainNum
+}
+const Division = () => {
+    return mainNum / mainNum
+}
+
+
+export { Addition, Subtraction, Multiplication, Division, mainNum }
